7106701eee8c8af9259c0e69ec7cda1342582b65
Edgar Ignacio Zavala Garduño <ignaciogard@gmail.com>
2019-01-21 18:15:18 -0600

    Segunda version con galeria

---
+++ animacion.js
@@ -0,0 +1,47 @@
+/*
+TIpos de datos
+numero 
+cadena
+booleano   TRUE O FALSE
+fechas 
+simbolo 
+objetos
+
+*/
+//************PARA MAS INFORMACION VISITA********************************
+// VISITIA LA PAGINA https://developer.mozilla.org/es/docs/Web/JavaScript/Guide
+
+
+
+
+var edad="Continua!!!!!";
+//alert(edad);
+
+
+var mensaje= "Solo va a aparecer en la consola de desarrollador del navegador";
+//console.log(mensaje);
+function saludo(argument) {
+	// body...
+
+	var uno="Que paso rey";
+
+//	document.write(uno);
+	//document.write(typeof uno);// typeof te va a decir que tipo de variable contiene uno
+
+}
+
+function fecha(){
+var fecha=Date();
+
+
+
+//document.write(fecha);
+//document.write(typeof fecha2); // TE VA A DECIR QUE ES UN OBJETO
+//EJEMPLOS DE OBJETOS
+
+
+
+}
+
+var fecha2= new Date();//ESTE ES UN OBJETO
+document.write(fecha2.getHours()+": ",fecha2.getMinutes() );
